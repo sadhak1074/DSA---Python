c63457cece3761288313707086e4ad74550a353a
sadhak1074 <sadhaksaini18@gmail.com.com>
2024-05-25 09:16:29 +0530

    Lists in Python

---
+++ list.py
@@ -0,0 +1,20 @@
+#creating python list
+list = ["1", "Sadhak", "@!#", "1.5"]
+print("\n Printing List")
+print(list) #printing created list
+
+#creating a multi-dimensional list
+list2 = [["1", "2"], ["Sadhak"]]
+print("\n Printing Multi-Dimensional List")
+print(list2)
+
+#accessing list elements by indexing
+print("\n Indexing")
+
+print(list[0])
+
+
+#accessing list elements by negative indexing
+print("\n Negative Indexing")
+
+print(list[-1])
