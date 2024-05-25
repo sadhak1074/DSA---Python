e1b86624943915ecb6c56031837b1bb8f6027d28
sadhak1074 <sadhaksaini18@gmail.com.com>
2024-05-25 11:40:22 +0530

    Add tuples.py with tuple operations

---
+++ sets.py
@@ -0,0 +1,12 @@
+# Creating a Set with
+# a mixed type of values
+# (Having numbers and strings)
+Set = set([1, 2, 'Geeks', 4, 'For', 6, 'Geeks'])
+print("\nSet with the use of Mixed Values")
+print(Set)
+
+print("\nElements of set: ")
+
+for i in Set:
+    print(i, end=" ")
+    
